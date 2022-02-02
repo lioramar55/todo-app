f612a8b3d8794b67b3a2d5bb2d30a2ed9d34ca92
Lior Refael Amar <41070999+lioramar55@users.noreply.github.com>
2022-02-03 00:12:47 +0200

    Initial commit, code works fine

---
+++ js/services/todo-service.js
@@ -0,0 +1,125 @@
+'use strict';
+
+const STORAGE_KEY = 'todosDB';
+var gTodos;
+var gFilterBy = 'ALL';
+var gSortBy;
+
+_createTodos();
+
+function getTodosForDisplay() {
+  _sortTodos(gSortBy);
+  if (gFilterBy === 'ALL') return gTodos;
+  return gTodos.filter(
+    (todo) => (todo.isDone && gFilterBy === 'DONE') || (!todo.isDone && gFilterBy === 'ACTIVE')
+  );
+}
+
+function removeTodo(todoId) {
+  const idx = gTodos.findIndex((todo) => todo.id === todoId);
+  gTodos.splice(idx, 1);
+  _saveTodosToStorage();
+}
+
+function toggleTodo(todoId) {
+  var todo = gTodos.find((todo) => todo.id === todoId);
+  todo.isDone = !todo.isDone;
+  _saveTodosToStorage();
+}
+
+function addTodo(txt, importance) {
+  const todo = _createTodo(txt, importance);
+  gTodos.unshift(todo);
+  _saveTodosToStorage();
+}
+
+function getTodosCount() {
+  return gTodos.length;
+}
+
+function getActiveTodosCount() {
+  const activeTodos = gTodos.filter((todo) => !todo.isDone);
+  return activeTodos.length;
+}
+
+function setFilter(filterBy) {
+  gFilterBy = filterBy;
+}
+
+function isFiltered() {
+  return gFilterBy === 'ALL';
+}
+
+function setSort(sortBy) {
+  gSortBy = sortBy;
+}
+
+function editTodoLocation(direction, idx) {
+  var temp = null;
+  console.log('direction', direction);
+  if (direction === 'up') {
+    temp = gTodos[idx];
+    gTodos[idx] = gTodos[idx - 1];
+    gTodos[idx - 1] = temp;
+    return;
+  } else {
+    temp = gTodos[idx];
+    gTodos[idx] = gTodos[idx + 1];
+    gTodos[idx + 1] = temp;
+  }
+}
+
+function _createTodos() {
+  gTodos = loadFromStorage(STORAGE_KEY);
+  if (!gTodos || !gTodos.length) {
+    gTodos = [
+      _createTodo('Learn HTML'),
+      _createTodo('Study CSS'),
+      _createTodo('Master Javascript'),
+    ];
+    _saveTodosToStorage();
+  }
+}
+
+function _createTodo(txt, importance = 1) {
+  const todo = {
+    id: _makeId(),
+    txt: txt,
+    isDone: false,
+    createdAt: Date.now(),
+    importance,
+  };
+  return todo;
+}
+
+function _makeId(length = 5) {
+  var txt = '';
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (var i = 0; i < length; i++) {
+    txt += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return txt;
+}
+
+function _saveTodosToStorage() {
+  saveToStorage(STORAGE_KEY, gTodos);
+}
+
+function _sortTodos(sortBy) {
+  if (gSortBy === 'None') return;
+  switch (sortBy) {
+    case 'CREATED':
+      gTodos = gTodos.sort((a, b) =>
+        b.createdAt > a.createdAt ? 1 : a.createdAt > b.createdAt ? -1 : 0
+      );
+      break;
+    case 'TXT':
+      gTodos.sort((a, b) => (a.txt > b.txt ? 1 : b.txt > a.txt ? -1 : 0));
+      break;
+    case 'IMPORTANCE':
+      gTodos.sort((a, b) =>
+        b.importance > a.importance ? 1 : a.importance > b.importance ? -1 : 0
+      );
+      break;
+  }
+}
---
+++ js/todo-controller.js
@@ -0,0 +1,80 @@
+'use strict';
+
+// called on body load
+function onInit() {
+  renderTodos();
+}
+
+function onRemoveTodo(ev, todoId) {
+  ev.stopPropagation();
+
+  if (confirm('Are you sure you want to delete this TODO?')) removeTodo(todoId);
+  renderTodos();
+}
+
+function renderTodos() {
+  // get todos from service
+  const todos = getTodosForDisplay();
+  var strHTMLs = ``;
+  if (todos.length) {
+    strHTMLs = todos.map((todo, idx) => {
+      return `<li class="${todo.isDone ? 'done' : ''}" onclick="onToggleTodo(event, '${todo.id}')">
+              ${todo.txt + createEditButtons(idx)}
+              <button class= "inline-btn" onclick="onRemoveTodo(event, '${todo.id}')">X</button>
+              (${todo.importance})
+            </li>`;
+    });
+    strHTMLs = strHTMLs.join('');
+  } else {
+    strHTMLs += `<h3>Add something to do...</h3>`;
+  }
+  document.querySelector('.todo-list').innerHTML = strHTMLs;
+
+  document.querySelector('.todos-total-count').innerText = getTodosCount();
+  document.querySelector('.todos-active-count').innerText = getActiveTodosCount();
+}
+
+function createEditButtons(idx) {
+  if (!isFiltered()) return '';
+  var strHTML = '';
+  if (idx > 0)
+    strHTML += `<button onclick="onEditLocation(event, 'up', ${idx})" class="inline-btn">↑</button>`;
+  if (idx < gTodos.length - 1)
+    strHTML += `<button onclick="onEditLocation(event, 'down', ${idx})" class="inline-btn">↓</button>`;
+  return strHTML;
+}
+
+function onEditLocation(ev, direction, idx) {
+  editTodoLocation(direction, idx);
+  ev.stopPropagation();
+  renderTodos();
+}
+
+function onToggleTodo(ev, todoId) {
+  toggleTodo(todoId);
+  ev.stopPropagation();
+
+  renderTodos();
+}
+
+function onAddTodo() {
+  const elTxt = document.querySelector('input[name=todoTxt]');
+  const importance = document.querySelector('.importance-level').value;
+  const txt = elTxt.value;
+  if (txt.length) {
+    addTodo(txt, importance);
+  }
+
+  elTxt.value = '';
+  renderTodos();
+}
+
+function onSetFilter(filterBy) {
+  setFilter(filterBy);
+  renderTodos();
+}
+
+function onSetSort(sortBy) {
+  setSort(sortBy);
+  renderTodos();
+}
